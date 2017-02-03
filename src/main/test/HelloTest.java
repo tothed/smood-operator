0bd8981a5406ce6590f9a24bf3b4ee449a781a65
tothed <edototh@centrum.sk>
2017-02-03 10:04:25 +0100

    Added computation

--- src/main/test/HelloTest.java
+++ src/main/test/HelloTest.java
@@ -5,5 +5,6 @@ public class HelloTest {
     @org.junit.Test
     public void doIt() throws Exception {
         Assert.assertTrue(Hello.doIt(4) > 4);
+        Assert.assertTrue(Hello.doIt(4) != 0);
     }
 }
