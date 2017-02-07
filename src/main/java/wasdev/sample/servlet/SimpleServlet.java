854afc3b378fdc0b3a21877f1851a50d73e3b208
S04BIT <vhj@fdjj>
2017-02-07 17:31:44 +0100

    run it from local

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -20,7 +20,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!!!" + "Do It (4) result is " + Hello.doIt(4));
+        response.getWriter().print("Hello World!" + "Do It (4) result is " + Hello.doIt(4));
     }
 
 }
