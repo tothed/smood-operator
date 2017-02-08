babee1b939fa15e352c6ab4d0cb24a45cfa52d5a
S04BIT <vhj@fdjj>
2017-02-08 18:11:29 +0100

    datasource

--- src/main/java/wasdev/sample/servlet/CloudantServlet.java
+++ src/main/java/wasdev/sample/servlet/CloudantServlet.java
@@ -3,12 +3,15 @@ package wasdev.sample.servlet;
 import com.cloudant.client.api.Database;
 import com.cloudant.client.api.views.AllDocsResponse;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Map;
 
 /**
@@ -18,16 +21,20 @@ import java.util.Map;
 public class CloudantServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    @Resource(name = "jdbc/mydb")
+    private DataSource db;
+
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Database client = CloudantClientMgr.getDB("sample_nosql_db");
-        AllDocsResponse resp = client.getAllDocsRequestBuilder().includeDocs(true).build().getResponse();
+
         response.setContentType("text/html");
-        for (Map map : resp.getDocsAs(Map.class)){
-            response.getWriter().println(map);
+        try {
+            response.getWriter().print(db.getConnection().getMetaData().getDatabaseProductName());
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
