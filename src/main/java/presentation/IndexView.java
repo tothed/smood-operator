5dbd507a6f8eddf9d783485b3690c17aa87dc164
lusaxxx <lusaxxx@gmail.com>
2017-02-08 21:27:05 +0100

    ChatBot

---
+++ src/main/java/presentation/IndexView.java
@@ -0,0 +1,60 @@
+package presentation;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by S2QAH9 on 8. 2. 2017.
+ */
+@Named
+@SessionScoped
+public class IndexView implements Serializable{
+
+    @PostConstruct
+    private void init(){
+        question = "Hi Peter! Nice weather today how is your mood?";
+        step=0;
+        selectedDomains= new ArrayList<>();
+    }
+
+    private String question;
+    private int step;
+    private List<String> selectedDomains;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void save(int step){
+        this.step=step+1;
+        if (this.step==1){
+            question ="Cause of the mood?";
+        } else {
+            question ="How do we know? Link...";
+        }
+    }
+
+    public void reset(){
+        init();
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public List<String> getSelectedDomains() {
+        return selectedDomains;
+    }
+
+    public void setSelectedDomains(List<String> selectedDomains) {
+        this.selectedDomains = selectedDomains;
+    }
+}
